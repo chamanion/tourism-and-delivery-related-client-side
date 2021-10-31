20a2b4129a5987d0aaf678139210a19c307591a6
Nipon Chakma <Niponchakma>
2021-10-31 21:08:06 +0600

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Getting Started with Create React App
 
+<h1>Delivery Service Website</h1>
+<h3>Live site Link :https://delivery-service-83590.web.app/</h3>
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+
+import { BrowserRouter,Switch, Route } from 'react-router-dom';
 import './App.css';
+import Booking from './Components/Booking/Booking';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import ManageServices from './Components/ManageServices/ManageServices';
+import PrivateRoute from './Components/ManageServices/PrivateRoute';
+import MyOrder from './Components/MyOrder/MyOrder';
+import NotFound from './Components/NotFound/NotFound';
+import Register from './Components/Register/Register';
+
+import Footer from './Components/Shared/Footer/Footer';
+import Header from './Components/Shared/Header/Header';
+import AuthProvider from './context/AuthProvider/AuthProvider';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <AuthProvider>
+    <BrowserRouter>
+     <Header></Header>
+     <Switch>
+       <Route exact path="/">
+         <Home></Home>
+       </Route>
+       <Route exact path="/home">
+         <Home></Home>
+       </Route>
+       <Route exact path="/home">
+         <Home></Home>
+       </Route>
+       <Route exact path="/myOrder">
+         <MyOrder></MyOrder>
+       </Route>
+       <PrivateRoute exact path="/service/:serviceId">
+         <Booking></Booking>
+       </PrivateRoute>
+       <Route exact path="/manageServices">
+        <ManageServices></ManageServices>
+       </Route>
+       <Route exact path="/login">
+        <Login></Login>
+       </Route>
+       <Route exact path="/register">
+        <Register></Register>
+       </Route>
+       <Route exact path="*">
+        <NotFound></NotFound>
+       </Route>
+     </Switch>
+     <Footer></Footer>
+     </BrowserRouter>
+     
+    </AuthProvider>
     </div>
   );
 }
